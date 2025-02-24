4a217f6cc6f02fe309bc52d11608cfdb78c62a61
bacnx <bacnx@outfiz.com>
2025-02-24 16:47:25 +0700

    hoàn thành html cơ bản

---
+++ script.js
@@ -0,0 +1,15 @@
+// Khai Báo biến
+var a = 5;
+var b = 6;
+let x = 10;
+let y = 20;
+let z = a+b+x+y;
+
+// khai báo hằng
+const c = 6;
+const d = 6;
+
+// Hiển thị kết quả
+console.log("Kết quả biến",z); // 41
+console.log("kết quản biến", a + b); // 11
+console.log("Kết quả hằng", c + d); // 11
